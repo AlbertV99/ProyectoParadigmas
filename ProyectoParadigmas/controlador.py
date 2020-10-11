eb691b96213f8d8fdae0c2dc42b25b1dc958d07b
Alberto Daniel Valdez Urquhart <38679181+AlbertV99@users.noreply.github.com>
2020-10-11 08:17:11 +0200

    Creacion de nuevos ficheros y modulo MVC

---
+++ ProyectoParadigmas/controlador.py
@@ -0,0 +1,14 @@
+
+class Controller(object):
+    opcionesUsuario = {""}
+    def __init__(self, model, view):
+        self.model = model
+        self.view = view
+        iniciar_sesion()
+
+    def iniciar_sesion():
+        #FUNCION EN LA CUAL SE VA A INSTANCIAR LA EMPRESA Y EL USUARIO QUE VA A ESTAR UTILIZANDO EL SISTEMA ASI COMO TAMBIEN SUS POSIBLES OPCIONES
+
+        pass
+
+    def agregar
